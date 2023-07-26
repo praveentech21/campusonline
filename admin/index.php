43478718a0256ff0f5a524c7f9b0016776981e92
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-07-26 15:55:39 +0530

    Dash Bord Page Ui Setted Shiva

--- admin/index.php
+++ admin/index.php
@@ -52,9 +52,190 @@
         <!-- Content Starts Here Shiva-->
           <div class="container-xxl flex-grow-1 container-p-y">
             <div class="row">
+                <div class="row">
+                <!-- Today Delivary Agent Report Starts Here Shiva -->
+                <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
+                  <div class="card">
+                    <h5 class="card-header">This Week Sales Report</h5>
+                    <div class="card-body">
+                      <div class="table-responsive text-nowrap">
+                        <table class="table table-bordered">
+                          <thead>
+                            <tr>
+                              <th>Date</th>
+                              <th>Day</th>
+                              <th>No of Order</th>
+                              <th>Sales</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Rama Seetha</strong></td>
+                              <td><a href="tel:9052727402">9052727402</a></td>
+                              <td><span class="badge bg-label-warning me-1"> Boxes</span></td>
+                              <td><span class="badge bg-label-primary me-1"> Boxes</span></td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!--/ Today Delivary Agent Report Ends Here Shiva -->
 
-              <!-- Write Your Code Here Shiva -->
-              
+
+                <!-- Data Cards Here Shiva -->
+                <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
+                  <div class="row">
+                  <div class="col-6 mb-4">
+                      <div class="card">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                              <img src="BHavani/img/icons/unicons/chart-success.png" alt="Credit Card" class="rounded" />
+                            </div>
+                          </div>
+                          <span class="d-block mb-1">Total Orders</span>
+                          <h3 class="card-title text-nowrap mb-2">5 Orders</h3>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-6 mb-4">
+                      <div class="card">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                              <img src="BHavani/img/icons/unicons/wallet-info.png" alt="Credit Card" class="rounded" />
+                            </div>
+                          </div>
+                          <span class="fw-semibold d-block mb-1">In Transition</span>
+                          <h3 class="card-title mb-2">5 Boxes</h3>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-6 mb-4">
+                      <div class="card">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                              <img src="Bhavani/img/icons/unicons/paypal.png" alt="Credit Card" class="rounded" />
+                            </div>
+                          </div>
+                          <span class="d-block mb-1">Not Picked Up</span>
+                          <h3 class="card-title text-nowrap mb-2">5 Boxes</h3>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-6 mb-4">
+                      <div class="card">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                              <img src="Bhavani/img/icons/unicons/cc-warning.png" alt="Credit Card" class="rounded" />
+                            </div>
+                          </div>
+                          <span class="fw-semibold d-block mb-1">In Transition</span>
+                          <h3 class="card-title mb-2">5 Boxes</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                <!--/ Data Cards Here Shiva -->
+                <hr class="my-4">
+              <!-- Bordered Table -->
+              <div class="card">
+                <h5 class="card-header">category Wise Sales</h5>
+                <div class="card-body">
+                  <div class="table-responsive text-nowrap">
+                    <table class="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Category Name</th>
+                          <th>Category ID</th>
+                          <th>No of Products </th>
+                          <th>No of Orders </th>
+                          <th>Sales</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td><strong>Angular Project</strong></td>
+                          <td>Albert Cook</td>
+                          <td>Albert Cook</td>
+                          <td><span class="badge bg-label-primary me-1">Active</span></td>
+                          <td><span class="badge bg-label-primary me-1">Active</span></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+              <!--/ Bordered Table -->
+              <hr class="my-4">
+              <!-- Bordered Table -->
+              <div class="card">
+                <h5 class="card-header">Coupon Wise Sales</h5>
+                <div class="card-body">
+                  <div class="table-responsive text-nowrap">
+                    <table class="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Coupon Name</th>
+                          <th>Coupon ID</th>
+                          <th>No of Orders</th>
+                          <th>Sale Amount</th>
+                          <th>Coupon Amount</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td><strong>Angular Project</strong></td>
+                          <td>Albert Cook</td>
+                          <td>Albert Cook</td>
+                          <td><span class="badge bg-label-primary me-1">Active</span></td>
+                          <td><span class="badge bg-label-primary me-1">Active</span></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+              <!--/ Bordered Table -->
+              <hr class="my-4">
+              <!-- Bordered Table -->
+              <div class="card">
+                <h5 class="card-header">Monthly Orders</h5>
+                <div class="card-body">
+                  <div class="table-responsive text-nowrap">
+                    <table class="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Date</th>
+                          <th>Orders</th>
+                          <th>Sales</th>
+                          <th>Discount</th>
+                          <th>Coupon</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td><strong>Angular Project</strong></td>
+                          <td>Albert Cook</td>
+                          <td>Albert Cook</td>
+                          <td><span class="badge bg-label-primary me-1">Active</span></td>
+                          <td><span class="badge bg-label-primary me-1">Active</span></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+              <!--/ Bordered Table -->
+
+
+
             </div>
           </div>
         <!-- Content Ends Here Shiva -->
